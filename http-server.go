aadaf051cea92fabcd6d20712787668a8aeeacc4
Kaushal Soni <kaushal.soni@searce.com>
2022-06-16 19:37:35 +0530

    added

--- http-server.go
+++ http-server.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello World from GO HTTP Server </h1>\n\n <b>Deploying Server version v1 on gke cluster </b>\n"))
+		w.Write([]byte("<h1>Hello World from GO HTTP Server </h1>\n\n <b>Deploying Server version v1 on gke </b>\n"))
 	})
 	http.ListenAndServe(":80", nil)
 }
